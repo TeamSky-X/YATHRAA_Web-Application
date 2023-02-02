b0a6066ea6f32f6fe6b94a1da845a4138a69e22f
TeamSky-X <123046846+TeamSky-X@users.noreply.github.com>
2023-02-02 13:52:15 +0530

    Update index1.php

--- index1.php
+++ index1.php
@@ -37,7 +37,7 @@ include('includes/config.php');
 <div class="banner">
     <div class="container">
         <h1 class="wow zoomIn animated animated" data-wow-delay=".5s"
-            style="visibility: visible; animation-delay: 0.5s; animation-name: zoomIn;"><b>SMART Journey<b></h1>
+            style="visibility: visible; animation-delay: 0.5s; animation-name: zoomIn;"><b><b></h1>
     </div>
 </div>
 
